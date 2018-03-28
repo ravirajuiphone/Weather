1bcf065a54f7ce16a0bb23b3d3ed8bdc9936d45d
Raviraju Vysyaraju <rvysyaraju@ivycomptech.com>
2018-03-28 12:04:14 +0530

    Added WeatherRouter unit test

---
+++ OpenweatherTests/WeatherRouterTest.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherRouterTest.swift
+//  OpenweatherTests
+//
+//  Created by Raviraju Vysyaraju on 28/03/2018.
+//  Copyright © 2018 Raviraju Vysyaraju. All rights reserved.
+//
+
+import XCTest
+
+@testable import Openweather
+class WeatherRouterTest: XCTestCase {
+    var navController: MockNavigationController!
+    var sut: MapViewController!
+    var router: WeatherWireframe!
+    
+    override func setUp() {
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+        navController = MockNavigationController(rootViewController: sut)
+        router = WeatherRouter(navigationVC: navController)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        sut = nil
+        navController = nil
+        router = nil
+    }
+    
+    func testPresentMapViewController() {
+        router.presentMapViewController()
+        XCTAssertNotNil(navController.pushedViewController as? MapViewController)
+    }
+    class MockNavigationController: UINavigationController {
+        var pushedViewController: UIViewController?
+        
+        override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+            pushedViewController = viewController
+            super.pushViewController(viewController, animated: false)
+        }
+    }
+    
+}
